6c1de238ce901a31c32aa45cdaa59d2248ee5ff8
Davyson Melo <davyson.melo@liferay.com>
2025-11-06 11:50:19 -0300

    LPD-70609 Improve user message balloon to show user profile picture

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/AIAssistantChat/components/UserMessageBalloon.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/AIAssistantChat/components/UserMessageBalloon.tsx
@@ -3,19 +3,35 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {getUserInfo} from '../services/getUserInfo';
+import Avatar from './Avatar';
 
 const UserChatItem: React.FC<{message: string}> = ({message}) => {
-	return (
-		<div className="d-flex justify-content-end">
-			<span>{message}</span>
+	const [user, setUser] = useState<any>(null);
 
-			<img
-				alt={Liferay.Language.get('user-profile-image')}
-				className="ml-2"
-				src=""
-				style={{borderRadius: 12, height: 24, width: 24}}
-			/>
+	useEffect(() => {
+		async function fetchUser() {
+			try {
+				const userInfo = await getUserInfo(
+					Liferay.ThemeDisplay.getUserId().toString()
+				);
+				setUser(userInfo);
+			}
+			catch (error) {
+				console.error('Error fetching user info:', error);
+			}
+		}
+
+		fetchUser();
+	}, []);
+
+	return (
+		<div className="align-items-center d-flex justify-content-end mb-2">
+			<span className="ml-2">{message}</span>
+
+			<Avatar image={user?.image} name={user?.name} />
 		</div>
 	);
 };
