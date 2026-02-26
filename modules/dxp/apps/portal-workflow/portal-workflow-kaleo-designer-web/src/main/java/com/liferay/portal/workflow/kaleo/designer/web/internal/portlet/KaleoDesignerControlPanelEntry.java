699bc920ba21e4f6469e365b7799b23669efb8c5
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-02-25 23:30:01 -0300

    WIP 2

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/KaleoDesignerControlPanelEntry.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/KaleoDesignerControlPanelEntry.java
@@ -7,10 +7,9 @@ package com.liferay.portal.workflow.kaleo.designer.web.internal.portlet;
 
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
-import com.liferay.portal.kernel.portlet.AdministratorControlPanelEntry;
+import com.liferay.portal.kernel.portlet.BaseControlPanelEntry;
 import com.liferay.portal.kernel.portlet.ControlPanelEntry;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.workflow.WorkflowControlPanelEntry;
 import com.liferay.portal.workflow.kaleo.designer.web.constants.KaleoDesignerPortletKeys;
 
 import org.osgi.service.component.annotations.Component;
@@ -22,23 +21,14 @@ import org.osgi.service.component.annotations.Component;
 	property = "jakarta.portlet.name=" + KaleoDesignerPortletKeys.KALEO_DESIGNER,
 	service = ControlPanelEntry.class
 )
-public class KaleoDesignerControlPanelEntry
-	extends AdministratorControlPanelEntry {
+public class KaleoDesignerControlPanelEntry extends BaseControlPanelEntry {
 
 	@Override
 	public boolean hasAccessPermission(
 			PermissionChecker permissionChecker, Group group, Portlet portlet)
 		throws Exception {
 
-		if (!super.hasAccessPermission(permissionChecker, group, portlet)) {
-			return true;
-		}
-
-		return _workflowControlPanelEntry.hasAccessPermission(
-			permissionChecker, group, portlet);
+		return true;
 	}
-
-	private final WorkflowControlPanelEntry _workflowControlPanelEntry =
-		new WorkflowControlPanelEntry();
 
 }
