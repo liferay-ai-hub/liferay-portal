946cf86e1116db72a510420ea532b02a9f6d84f0
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-03-09 17:53:49 -0300

    LPD-75170 refactor: rename ai-hub-client-api to ai-hub-cell-api

--- modules/apps/ai-hub-cell/ai-hub-cell-api/src/main/java/com/liferay/ai/hub/cell/configuration/AIHubConfiguration.java
+++ modules/apps/ai-hub-cell/ai-hub-cell-api/src/main/java/com/liferay/ai/hub/cell/configuration/AIHubConfiguration.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.ai.hub.client.configuration;
+package com.liferay.ai.hub.cell.configuration;
 
 import aQute.bnd.annotation.metatype.Meta;
 
@@ -18,7 +18,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 	scope = ExtendedObjectClassDefinition.Scope.COMPANY
 )
 @Meta.OCD(
-	id = "com.liferay.ai.hub.client.configuration.AIHubConfiguration",
+	id = "com.liferay.ai.hub.cell.configuration.AIHubConfiguration",
 	localization = "content/Language", name = "ai-hub-configuration-name"
 )
 public interface AIHubConfiguration {
