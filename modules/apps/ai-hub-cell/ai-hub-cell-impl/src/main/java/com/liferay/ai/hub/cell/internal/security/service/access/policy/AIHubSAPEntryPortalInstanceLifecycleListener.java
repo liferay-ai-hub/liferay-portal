1e5c1f81322c99d6c8ca4c48f0743e68829108d3
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-03-09 18:11:18 -0300

    LPD-75170 refactor: rename ai-hub-client-impl to ai-hub-cell-impl

--- modules/apps/ai-hub-cell/ai-hub-cell-impl/src/main/java/com/liferay/ai/hub/cell/internal/security/service/access/policy/AIHubSAPEntryPortalInstanceLifecycleListener.java
+++ modules/apps/ai-hub-cell/ai-hub-cell-impl/src/main/java/com/liferay/ai/hub/cell/internal/security/service/access/policy/AIHubSAPEntryPortalInstanceLifecycleListener.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.ai.hub.client.internal.security.service.access.policy;
+package com.liferay.ai.hub.cell.internal.security.service.access.policy;
 
 import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
 import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
