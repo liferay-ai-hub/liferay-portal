d49b363893b3f42d059b57e36e7c1a9934751b55
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-03-13 10:46:05 -0300

    LPD-75170 refactor: move /ai-hub/ai-hub-web to /ai-hub-nexus/ai-hub-nexus-web

---
+++ modules/dxp/apps/ai-hub-nexus/ai-hub-nexus-web/src/main/java/com/liferay/ai/hub/nexus/web/internal/constants/AIHubNexusFDSNames.java
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.nexus.web.internal.constants;
+
+/**
+ * @author Feliphe Marinho
+ */
+public class AIHubNexusFDSNames {
+
+	public static final String AGENT_DEFINITIONS =
+		AIHubNexusWebConstants.BUNDLE_SYMBOLIC_NAME + "-agentDefinitions";
+
+}
