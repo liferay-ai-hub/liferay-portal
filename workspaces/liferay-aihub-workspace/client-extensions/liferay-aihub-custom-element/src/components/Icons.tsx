de6711383ed69190df78ca8d9ba9377c1350fd5d
Rafael Uen <rafael.uen@liferay.com>
2026-04-10 17:13:22 -0300

    LPD-85353 Add SVG icons

--- workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-custom-element/src/components/Icons.tsx
+++ workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-custom-element/src/components/Icons.tsx
@@ -3,34 +3,39 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayIcon, {ClayIconSpriteContext} from '@clayui/icon';
 import React from 'react';
 
-import {getSpritemapURL} from '../api';
-
-const WrappedClayIcon = ({symbol}: {symbol: string}) => (
-	<ClayIconSpriteContext.Provider value={getSpritemapURL()}>
-		<ClayIcon symbol={symbol} />
-	</ClayIconSpriteContext.Provider>
-);
-
 export function BotIcon() {
-	return <WrappedClayIcon symbol="chatbot" />;
+	return (
+		<svg viewBox="0 0 24 24">
+			<path d="M12 2a2 2 0 012 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 017 7v1a3 3 0 01-3 3H6a3 3 0 01-3-3v-1a7 7 0 017-7h1V5.73A2 2 0 0112 2zm-4 9a2 2 0 100 4 2 2 0 000-4zm8 0a2 2 0 100 4 2 2 0 000-4z" />
+		</svg>
+	);
 }
 
 export function ChatIcon() {
-	return <WrappedClayIcon symbol="comments" />;
+	return (
+		<svg viewBox="0 0 24 24">
+			<path d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H6l-2 2V4h16v12z" />
+		</svg>
+	);
 }
 
 export function CloseIcon() {
-	return <WrappedClayIcon symbol="times" />;
+	return (
+		<svg viewBox="0 0 16 16">
+			<path d="M9.41 8l4.3-4.29a1 1 0 10-1.42-1.42L8 6.59l-4.29-4.3a1 1 0 00-1.42 1.42L6.59 8l-4.3 4.29a1 1 0 101.42 1.42L8 9.41l4.29 4.3a1 1 0 001.42-1.42z" />
+		</svg>
+	);
 }
 
 export function ErrorIcon() {
 	return (
-		<span className="text-danger">
-			<WrappedClayIcon symbol="exclamation-full" />
-		</span>
+		<svg viewBox="0 0 16 16">
+			<circle cx="8" cy="8" fill="#da1414" r="8" />
+
+			<path d="M7 4h2v5H7zm0 6h2v2H7z" fill="#fff" />
+		</svg>
 	);
 }
 
