bc0365ee9042c6d4dba1be45df03d623e7ac0c77
Jose Abelenda <jose.abelenda@liferay.com>
2026-04-13 15:54:10 -0300

    LPD-85353 SF

--- workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-custom-element/src/components/ChatWidget.tsx
+++ workspaces/liferay-aihub-workspace/client-extensions/liferay-aihub-custom-element/src/components/ChatWidget.tsx
@@ -65,7 +65,7 @@ export default function ChatWidget({widgetConfiguration}: ChatWidgetProps) {
 
 			setMessages((prev) => [
 				...prev,
-				{sender: 'assistant', text: dataJSON['data']},
+				{sender: 'assistant', text: dataJSON.data},
 			]);
 
 			setGenerating(false);
