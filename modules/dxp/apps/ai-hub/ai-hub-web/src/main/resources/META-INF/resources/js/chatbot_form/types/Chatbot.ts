743135795e249ac4e3c2e2615b9a8e42ff534c7a
Davyson Melo <davyson.melo@liferay.com>
2026-04-28 11:37:13 -0300

    LPD-86205 feat: support file upload in chatbot type

--- modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/chatbot_form/types/Chatbot.ts
+++ modules/dxp/apps/ai-hub/ai-hub-web/src/main/resources/META-INF/resources/js/chatbot_form/types/Chatbot.ts
@@ -3,9 +3,15 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+export type CompanyLogoUpload = {
+	fileBase64: string;
+	mimeType?: string;
+	name: string;
+};
+
 export type Chatbot = {
 	active: boolean;
-	companyLogo: number | null | undefined;
+	companyLogo?: number | CompanyLogoUpload | null;
 	companyLogoFileName?: string;
 	description: string;
 	externalReferenceCode: string;
